8bed160c58ca4d8cb1624fe88ed23d15aa8a7239
ankita <ankita>
2019-12-04 10:34:35 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,238 @@
 import React, { Component} from 'react';
 import './App.css';
 import { debounce } from "debounce";
-import { link } from 'fs';
+import { data} from "./config";
 class App  extends Component{
 
   scrollTop=0;
   anchorClicked=false;
   isScrolled=false;
   activeIndex=0;
-  links=[{name:'home',id:0},{name:'about',id:1},{name:'services',id:2},{name:'team',id:3},{name:'projects',id:4},{name:'testimonials',id:5}];
-refsCollection:any;
+  links=[{name:'home',id:0,isPageInView:false},{name:'about',id:1,isPageInView:false},{name:'services',id:2,isPageInView:false},{name:'team',id:3,isPageInView:false},{name:'projects',id:4,isPageInView:false},{name:'testimonials',id:5,isPageInView:false}];
+  refsCollection:any;
+swiper:any;
+state:any;
+
  constructor(props: Readonly<{}>){
    super(props);
  this.refsCollection=[];
- 
+ this.swiper=React.createRef();
+ this.state={animatePage:false};
+
+
   }
+  
 
 
   componentDidMount() {
-  document.addEventListener('scroll',this.handleScroll);
-
-
+this.swiper.addEventListener('scroll',this.handleScroll);
+ 
 }
 
-handleScroll = debounce((e:any) => { 
-  const scrollTop=e.target.scrollingElement.scrollTop;
+handleScroll = debounce((e:any) =>{ 
+  this.setState({animatePage:false});
+  const scrollTop=e.srcElement.scrollTop;
   if(!this.anchorClicked && this.activeIndex===0){
     this.refsCollection[this.activeIndex].scrollIntoView({behavior: "smooth", block: "start"});
   }
   if(!this.anchorClicked){
     if(this.scrollTop>scrollTop){
-       this.goToPage('prev');
+	   this.goToPage('prev');
         
     }
     else{
       this.goToPage('next');
     }
-   
    }
    this.anchorClicked=!this.anchorClicked;
   this.scrollTop=scrollTop;
- 
-}, 100);
+  this.setState({animatePage:true});
+
+}, 50);
   
 
 
 goToPage(position:string){
-  console.log(position);
-  console.log(this.activeIndex);
   if(position==='prev'&&this.activeIndex>0){
     this.activeIndex--;
+    for(let i=0;i<this.links.length;i++){
+      this.links[i].isPageInView=i===this.activeIndex?true:false;
+    }
     window.location.hash=`#${this.links[this.activeIndex].name}`;
-    this.refsCollection[this.activeIndex].scrollIntoView({behavior: "smooth", block: "start"});
-
-  }else if(position==='next'&&this.activeIndex<this.links.length){
+    this.refsCollection[this.activeIndex].scrollIntoView(true);
+ 
+  }else if(position==='next'&&this.activeIndex<this.links.length-1){
     this.activeIndex++;
+  for(let i=0;i<this.links.length;i++){
+    this.links[i].isPageInView=i===this.activeIndex?true:false;
+  }
     window.location.hash=`#${this.links[this.activeIndex].name}`;
-    this.refsCollection[this.activeIndex].scrollIntoView({behavior: "smooth", block: "start"});
+    this.refsCollection[this.activeIndex].scrollIntoView(true);
+   
   }
 }
-handleClick(anchorClicked: boolean){
+handleClick(anchorClicked: boolean,clickedIndex:number){
+  for(let i=0;i<this.links.length;i++){
+    this.links[i].isPageInView=i===clickedIndex?true:false;
+  }
 this.anchorClicked=anchorClicked;
+this.activeIndex=clickedIndex;
 }
 
 
 render(){
+ 
+
+
   return (
     <div className="App"  ref="app">
-     <div className="navbar">
-      <a href="#home" onClick={()=>this.handleClick(true)}>home</a>
-      <a href="#about" onClick={()=>this.handleClick(true)}>about</a>
-      <a href="#services" onClick={()=>this.handleClick(true)}>services</a>
-      <a href="#team" onClick={()=>this.handleClick(true)}>team</a>
-      <a href="#projects"onClick={()=>this.handleClick(true)}>projects</a>
-      <a href="#testimonials"onClick={()=>this.handleClick(true)}>testimonials</a>
-     </div>
+     	<div className="navbar">
+			 <div>
+			 <a href="#home" onClick={()=>this.handleClick(true,0)}>Home</a>
 
-{this.links.map((link,index)=>{
-  return <div className="wrapper"  key={index}>
-  
-    <div className="view"  id={link.name}  ref={(el)=>{this.refsCollection[index]=el}} >
-<div className="view-header">{link.name}</div>
-  </div>
-</div>
-})}
+			 </div>
+			 <div>
+			 <a href="#about" onClick={()=>this.handleClick(true,1)}>About Us</a>
 
-        </div>
+			 </div>
+			 <div>
+			 <a href="#services" onClick={()=>this.handleClick(true,2)}>Services</a>
+
+			 </div>
+			 <div>
+			 <a href="#team" onClick={()=>this.handleClick(true,3)}>Team</a>
+
+			 </div>
+			 <div>
+			 <a href="#projects"onClick={()=>this.handleClick(true,4)}>Projects</a>
+
+			 </div>
+
+    	</div>
+
+		<div className="page-cover"></div>
+    	<div className="section-home">
+    		<div className="main-container"></div>
+    		<div className="swiper-container" id="swiper" ref={(el)=>{this.swiper=el}}>
+     	 		<div className={"swiper-slide home-slide center-vh "+(this.links[0].isPageInView?'appear':'disappear')} id={this.links[0].name}  ref={(el)=>{this.refsCollection[0]=el}}>
+     		 		<div className="slide-container home-container">
+  						<h1 className={this.state.animatePage?'home-header anim-1':'home-header'}>
+					      RADON TECH
+  						</h1>
+						  <div className={this.state.animatePage?'home-sub-header anim-2':'home-sub-header'}>
+					    We are creative experts, build your company with us. Today’s progress was yesterday’s plan.
+						  </div>
+					</div>
+      			</div>
+	  			<div className={"swiper-slide about-slide center-vh "+(this.links[0].isPageInView?'appear':'disappear')}  id={this.links[1].name}  ref={(el)=>{this.refsCollection[1]=el}}>
+     				 <div className="slide-container about-container">
+ 							 <div>
+								<div className="about-header">
+									About Us
+								</div>
+							</div>
+ 							 <div className="about-sub-section">
+								<div className={this.state.animatePage?'left-col anim-3':'left-col'}>
+									Our mission gives solution
+								</div>
+								<div className="right-col">
+									<div className={this.state.animatePage?'right-col-text anim-2':'right-col-text'}>
+									<p>
+										{data.aboutUs.subHeader[0]}
+									</p>
+									<p>
+										{data.aboutUs.subHeader[1]}
+
+									</p>
+								</div>
+								</div>
+							</div>
+					</div>
+    			</div>
+				<div className="swiper-slide"  id={this.links[2].name}  ref={(el)=>{this.refsCollection[2]=el}}>
+      				<div className="slide-container services-container">
+						  <div > 
+						  <div className="services-header">
+							  {data.services.header}
+      					</div>
+						  <div className="services-subheader">
+								{data.services.subHeader}
+						  </div>
+						  </div>
+      					
+				  		<div className={'services-description'}>
+							  <div className="description-container">
+							  <div className="description-wrapper">
+								  <div className="description-header">
+										{data.services.description[0][0]}
+								  </div>
+									<div className="description-subheader">
+										{data.services.description[0][1]}
+
+									</div>
+							  </div>
+							  <div className="description-wrapper">
+								  <div className="description-header">
+										{data.services.description[1][0]}
+								  </div>
+									<div className="description-subheader">
+										{data.services.description[1][1]}
+
+									</div>
+							  </div>
+							  </div>
+							  <div className="description-container">
+							  <div className="description-wrapper">
+								  <div className="description-header"> 
+										{data.services.description[2][0]}
+								  </div>
+									<div className="description-subheader">
+										{data.services.description[2][1]}
+									</div>
+							  </div>
+							  <div className="description-wrapper">
+								  <div className="description-header">
+										{data.services.description[3][0]}
+								  </div>
+									<div className="description-subheader">
+										{data.services.description[3][1]}
+									</div>
+							  </div>
+							  </div>
+							  
+							  
+
+              			</div>
+
+      				</div>
+    			</div>
+				<div className="swiper-slide"  id={this.links[3].name}  ref={(el)=>{this.refsCollection[3]=el}}>
+      				<div className="slide-container team-container">
+      					<h1 className="about-header">
+						  {this.links[3].name}
+      					</h1>
+						  <div className="home-sub-header">
+            				    We are creative experts, build your company with us. Today’s progress was yesterday’s plan.
+           				   </div>
+
+      				</div>
+    			</div>
+				<div className="swiper-slide"  id={this.links[4].name}  ref={(el)=>{this.refsCollection[4]=el}}>
+      				<div className="slide-container project-container">
+      					<h1 className="about-header">
+						  {this.links[4].name}
+      					</h1>
+						<div className="home-sub-header">
+        	    		    We are creative experts, build your company with us. Today’s progress was yesterday’s plan.
+        	    		</div>
+      				</div>
+    			</div>
+			
+      		</div>
+    	</div>
+	</div>
   )
-  
 }
-
-  
-
 }
 
 
---
+++ src/config.ts
@@ -0,0 +1,21 @@
+
+export const data={
+	home:
+	{
+   		header:''
+	},
+	aboutUs:{
+		header:'ABOUT US',
+		subHeader:['Radon Tech works on the principle of completeness! We come a full circle in building your product right from the back end to the front end, be it web or mobile applications.','We work on the latest, cutting-edge, tech stack to deliver your product in an efficient, performant and maintainable way. We like to make our code as beautiful as your user experience. :)']
+	},
+	services:{
+		header:'SERVICES',
+		subHeader:'Our awesome services which we provide for your needs',
+		description:[
+					['Frontend','Be it React, Vue or Angular we have experience in building scalable applications.'],
+					['Backend','Our developers are proficient in Node.js, Go and .NET Core coupled with SQL and NoSql databases.'],
+					['Mobile','We build apps using React Native and Ionic.'],
+					['Data','Our python developers are quick in analyzing your data using Pandas and Numpy. We generate your reports using Tableau, PowerBI and Metabase.']
+				]
+			}	
+}
